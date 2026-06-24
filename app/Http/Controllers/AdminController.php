0cd12aa6e6c4d4b8c0cd72a176ce24e00e0ae2dd
Jayshri <jayshri@gmail.com>
2026-06-24 13:09:21 +0530

    Admin Brand Page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Brand;
 
 class AdminController extends Controller
 {
     public function index(){
         return view('admin.index');
+    }
+    
+    public function brands(){
+        $brands= Brand::orderBy('id','DESC')->paginate(10);
+
+        return view('admin.brands',compact('brands'));
     }
 }
